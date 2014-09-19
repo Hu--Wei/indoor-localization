07ed9a0afd3dcb7d5a72432a432b10f62ce7f470
KongLinghang <Epsilon_F@163.com>
2014-09-19 11:14:23 +0800

    progress bar

--- JsonDemo/InputActivity.java
+++ JsonDemo/InputActivity.java
@@ -16,6 +16,7 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -28,67 +29,74 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.net.wifi.*;
 
 public class InputActivity extends Activity {
-	//server IP, for example 101.5.155.101:8080
+	// server IP, for example 101.5.155.101:8080
 	private EditText mEtServerIP;
-	//position coordinate
+	// position coordinate
 	private EditText inX;
 	private EditText inY;
-	//position label
+	// position label
 	private EditText inPos;
-	//the angle between the map and north (i.e.: east = 90)
+	// the angle between the map and north (i.e.: east = 90)
 	private TextView mWifiResult;
 	private Button mBtnWifi;
 	private WifiTask wifiTask;
 
+	// progress bar
+	private ProgressBar bar2;
+	private double expectedTime = 20000;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		//显示app的界面
+
+		// 显示app的界面
 		setContentView(R.layout.input_main);
-		
-		//获取app的组件信息
+
+		// 获取app的组件信息
 		mEtServerIP = (EditText) findViewById(R.id.et_serverip2);
 		inX = (EditText) findViewById(R.id.editX);
 		inY = (EditText) findViewById(R.id.editY);
 		inPos = (EditText) findViewById(R.id.editPosition);
-		
+
 		mWifiResult = (TextView) findViewById(R.id.wifi_result);
-		
+
 		mBtnWifi = (Button) findViewById(R.id.btn_wifi);
-			    
+		// progress bar
+		bar2 = (ProgressBar) findViewById(R.id.bar2);
+
 		mWifiResult.setMovementMethod(ScrollingMovementMethod.getInstance());
-				
+
 		mBtnWifi.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v){
+			public void onClick(View v) {
+				bar2.setVisibility(View.VISIBLE);
 				wifiTask = new WifiTask();
 				wifiTask.execute();
-			}	
+			}
 		});
-		
+
 	}
-	
-	
+
 	/**
-	 *  数据库输入线程的具体任务，包括：
-	 *  1. 连接servlet
-	 *  2. 将用户信息通过param用HttpPost发送给server
+	 * 数据库输入线程的具体任务，包括： 1. 连接servlet 2. 将用户信息通过param用HttpPost发送给server
 	 */
-	
-	class WifiTask extends AsyncTask<Void, Integer, String> 
-	{
-		final public double LN10 = Math. log(10);
+
+	class WifiTask extends AsyncTask<Void, Integer, String> {
+		final public double LN10 = Math.log(10);
 		final public int NUMBER_OF_TESTS = 5;
 		private int count = 0;
 		private WifiManager wifi;
 		private String wifiResult = "";
 		private Map<String, Double> map = new HashMap<String, Double>();
 		private List<ScanResult> results = null;
+		private int progress;
+		private long startTime, duration;
+
 		private void updateMap() {
 			results = wifi.getScanResults();
 			for (ScanResult result : results) {
@@ -96,14 +104,16 @@ public class InputActivity extends Activity {
 					Double sum = map.get(result.BSSID);
 					sum += Math.exp(result.level * LN10 / 10);
 					map.put(result.BSSID, sum);
-				}
-				else 
+				} else
 					map.put(result.BSSID, Math.exp(result.level * LN10 / 10));
-			}								
+			}
 		}
+
 		@Override
 		protected String doInBackground(Void... params) {
-			publishProgress(0);
+			startTime = System.currentTimeMillis();
+			progress = 0;
+			publishProgress(0, 0);
 			WifiReceiver receiver = new WifiReceiver();
 			registerReceiver(receiver, new IntentFilter(
 					WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
@@ -112,30 +122,32 @@ public class InputActivity extends Activity {
 			map.clear();
 			for (int i = 0; i < NUMBER_OF_TESTS; i++) {
 				wifi.startScan();
-				if (i > 0) 
+				if (i > 0)
 					updateMap();
 				while (count <= i) {
 					try {
 						Thread.sleep(100);
-					}
-					catch (InterruptedException e) {
+						progress += 100;
+						publishProgress(progress, i);
+					} catch (InterruptedException e) {
 					}
 				}
 				if (i == NUMBER_OF_TESTS - 1)
 					updateMap();
-				publishProgress(i + 1);
+				progress = (int) (expectedTime * (i + 1) / NUMBER_OF_TESTS);
+				publishProgress(progress, i + 1);
 			}
 
-			//通过HttpPost连接servlet
+			// 通过HttpPost连接servlet
 			HttpClient hc = new DefaultHttpClient();
-			String address = "http://" + (mEtServerIP).getText().toString() + ":8080/ServerJsonDemo/servlet/JsonServlet";
+			String address = "http://" + (mEtServerIP).getText().toString()
+					+ ":8080/ServerJsonDemo/servlet/JsonServlet";
 			HttpPost hp = new HttpPost(address);
 			String strPos = inPos.getText().toString();
 			String strX = inX.getText().toString();
 			String strY = inY.getText().toString();
-			//String strZ = inZ.getText().toString();
-			
-			//通过param发送参数给servlet
+
+			// 通过param发送参数给servlet
 			List<NameValuePair> param = new ArrayList<NameValuePair>();
 			param.add(new BasicNameValuePair("type", "input"));
 			param.add(new BasicNameValuePair("pos", strPos));
@@ -146,18 +158,20 @@ public class InputActivity extends Activity {
 
 			Iterator<Map.Entry<String, Double>> it = map.entrySet().iterator();
 			while (it.hasNext()) {
-				Map.Entry<String, Double> entry = (Map.Entry<String, Double>) it.next();
-				param.add(new BasicNameValuePair((idx++) + "", entry.getKey() + "&" + 
-						10 * Math.log10(entry.getValue() / NUMBER_OF_TESTS)));
+				Map.Entry<String, Double> entry = (Map.Entry<String, Double>) it
+						.next();
+				param.add(new BasicNameValuePair((idx++) + "", entry.getKey()
+						+ "&" + 10
+						* Math.log10(entry.getValue() / NUMBER_OF_TESTS)));
 			}
 
-			try{
-				hp.setEntity(new UrlEncodedFormEntity(param, "utf-8")); 
+			try {
+				hp.setEntity(new UrlEncodedFormEntity(param, "utf-8"));
 				// 发送请求
 				HttpResponse response = hc.execute(hp);
 				// 返回200即请求成功
 				if (response.getStatusLine().getStatusCode() == 200) {
-					//数据写入成功
+					// 数据写入成功
 					wifiResult += "上传成功";
 				} else {
 					wifiResult += "连接失败";
@@ -172,36 +186,41 @@ public class InputActivity extends Activity {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			bar2.setProgress(100);
+			duration = System.currentTimeMillis() - startTime;
+			expectedTime = duration;
 			return wifiResult;
 		}
-		
+
 		@Override
 		protected void onPostExecute(String result) {
 			// TODO Auto-generated method stub
 			super.onPostExecute(result);
-			//在文本框中显示wifi信息
+			// 在文本框中显示wifi信息
 			mWifiResult.setText(result);
+			bar2.setVisibility(View.GONE);
 		}
-		
+
 		@Override
-		protected void onProgressUpdate(Integer ... values) {
+		protected void onProgressUpdate(Integer... values) {
 			super.onProgressUpdate(values);
-			String s = values[0] > 1? "s": "";
-			mWifiResult.setText(values[0] + " scan" + s + " finished!");
+			mWifiResult.setText("已完成" + values[1] + "次扫描");
+			bar2.setProgress((int) (values[0] / expectedTime * 100));
 		}
-		
+
 		class WifiReceiver extends BroadcastReceiver {
-	        @Override
-	        public void onReceive(Context context, Intent intent) {             
-	        	int k = 1;
-	        	wifiResult += "Scan #" + (count + 1) + "\n";
-	            final List<ScanResult> results= wifi.getScanResults();//list of access points from the last scan
-	                for(final ScanResult result : results){
-	                	wifiResult += (k++) + " : " + result.BSSID + " " 
-	                			+ result.SSID + " " + result.level + "\n";
-	            }
-	            count++;
-	        }
-	    }
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				int k = 1;
+				wifiResult += "Scan #" + (count + 1) + "\n";
+				final List<ScanResult> results = wifi.getScanResults();
+				// list of access points from the last scan
+				for (final ScanResult result : results) {
+					wifiResult += (k++) + " : " + result.BSSID + " "
+							+ result.SSID + " " + result.level + "\n";
+				}
+				count++;
+			}
+		}
 	}
 }
--- JsonDemo/QueryActivity.java
+++ JsonDemo/QueryActivity.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.ParseException;
@@ -40,106 +41,108 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.net.wifi.*;
 
-public class QueryActivity extends Activity implements SensorEventListener{
-	//server IP, for example 101.5.155.101:8080
+public class QueryActivity extends Activity implements SensorEventListener {
+	// server IP, for example 101.5.155.101:8080
 	private EditText mEtServerIP;
-	//the angle between the map and north (i.e.: east = 90)
+	// the angle between the map and north (i.e.: east = 90)
 	private EditText inOrient;
-	
+
 	private TextView mTvResult;
-	
+
 	private String mStrResult;
-	
+
 	private Button mBtnLogin;
 	private Button mBtnTrain;
-	
+
 	private MyTask mTask;
 	private TrainTask trainTask;
-	
+
 	private float orientOfMap;
 	private float orientOfDevice;
-	
-	//orientation sensor
+
+	// orientation sensor
 	private SensorManager mSensorManager;
 	private Sensor mOrientation;
-	
+
+	// progress bar
+	private ProgressBar bar1;
+	private double expectedTime = 4000;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		//显示app的界面
+
+		// 显示app的界面
 		setContentView(R.layout.query_main);
-		
-		//获取app的组件信息
+
+		// 获取app的组件信息
 		mEtServerIP = (EditText) findViewById(R.id.et_serverip);
 		inOrient = (EditText) findViewById(R.id.editOrient);
 		mTvResult = (TextView) findViewById(R.id.tv_result);
 		mBtnLogin = (Button) findViewById(R.id.btn_login);
 		mBtnTrain = (Button) findViewById(R.id.btn_train);
-		
-	    //orientation sensor
-	    mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-	    mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-	    //end of orientation
-	    
+
+		// orientation sensor
+		mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+		mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+		// end of orientation
+
+		// progress bar
+		bar1 = (ProgressBar) findViewById(R.id.bar1);
+
 		/**
-		 *  处理btn_login的响应任务，启动一个MyTask，处理数据库的查询
+		 * 处理btn_login的响应任务，启动一个MyTask，处理数据库的查询
 		 */
 		mBtnLogin.setOnClickListener(new OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				//mStrName = (mEtName).getText().toString();
+				// mStrName = (mEtName).getText().toString();
 				// AsyncTask异步任务开始
+				bar1.setVisibility(View.VISIBLE);
 				mTask = new MyTask();
 				mTask.execute();
 			}
 		});
-		
+
 		mBtnTrain.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v){
+			public void onClick(View v) {
 				trainTask = new TrainTask();
 				trainTask.execute();
-			}	
+			}
 		});
 	}
-	
-	
-	class TrainTask extends AsyncTask<String, Void, String>
-	{
+
+	class TrainTask extends AsyncTask<String, Void, String> {
 
 		@Override
-		protected String doInBackground(String... params) 
-		{
+		protected String doInBackground(String... params) {
 			// TODO Auto-generated method stub
 			HttpClient hc = new DefaultHttpClient();
 			// 这里是服务器的IP，不要写成localhost了，即使在本机测试也要写上本机的IP地址，localhost会被当成模拟器自身的
-			String address = "http://" + (mEtServerIP).getText().toString() + ":8080/ServerJsonDemo/servlet/JsonServlet";
+			String address = "http://" + (mEtServerIP).getText().toString()
+					+ ":8080/ServerJsonDemo/servlet/JsonServlet";
 			HttpPost hp = new HttpPost(address);
 			List<NameValuePair> param = new ArrayList<NameValuePair>();
 			param.add(new BasicNameValuePair("type", "test"));
 			// String str=null;
-			try
-			{
-				hp.setEntity(new UrlEncodedFormEntity(param, "utf-8")); 
+			try {
+				hp.setEntity(new UrlEncodedFormEntity(param, "utf-8"));
 				// 发送请求
 				HttpResponse response = hc.execute(hp);
 				// 返回200即请求成功
-				if (response.getStatusLine().getStatusCode() == 200) 
-				{	
+				if (response.getStatusLine().getStatusCode() == 200) {
 					System.out.println("训练成功");
-				} 
-				else 
-				{
+				} else {
 					System.out.println("连接失败");
 				}
-			}
-			catch (UnsupportedEncodingException e) {
+			} catch (UnsupportedEncodingException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} catch (ClientProtocolException e) {
@@ -149,7 +152,7 @@ public class QueryActivity extends Activity implements SensorEventListener{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			//返回包好user信息的str
+			// 返回包好user信息的str
 			return null;
 		}
 	}
@@ -157,42 +160,49 @@ public class QueryActivity extends Activity implements SensorEventListener{
 	class MyTask extends AsyncTask<Void, Integer, String> {
 		private WifiManager wifi;
 		private List<ScanResult> results = null;
-		boolean finished = false;
+		private boolean finished = false;
+		private int progress;
+		private long startTime, duration;
 
 		@Override
 		protected String doInBackground(Void... params) {
-			publishProgress(0);
+			startTime = System.currentTimeMillis();
+			progress = 0;
+			publishProgress(0, 0);
 			WifiReceiver receiver = new WifiReceiver();
 			registerReceiver(receiver, new IntentFilter(
 					WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
 			wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
 			wifi.startScan();
-			
+
 			while (!finished) {
 				try {
 					Thread.sleep(100);
-				} 
-				catch (InterruptedException e) {
+					progress += 100;
+					publishProgress(progress, 0);
+				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
-			publishProgress(1);
+			publishProgress(progress, 1);
 			// TODO Auto-generated method stub
 			HttpClient hc = new DefaultHttpClient();
 			// 这里是服务器的IP，不要写成localhost了，即使在本机测试也要写上本机的IP地址，localhost会被当成模拟器自身的
-			String address = "http://" + (mEtServerIP).getText().toString() + ":8080/ServerJsonDemo/servlet/JsonServlet";
+			String address = "http://" + (mEtServerIP).getText().toString()
+					+ ":8080/ServerJsonDemo/servlet/JsonServlet";
 			HttpPost hp = new HttpPost(address);
-			List<NameValuePair>param = new ArrayList<NameValuePair>();
+			List<NameValuePair> param = new ArrayList<NameValuePair>();
 			param.add(new BasicNameValuePair("type", "search"));
 			results = wifi.getScanResults();
 			param.add(new BasicNameValuePair("num", results.size() + ""));
 			int idx = 0;
 			for (ScanResult result : results) {
-				param.add(new BasicNameValuePair((idx++) + "", result.BSSID + "&" + result.level));
+				param.add(new BasicNameValuePair((idx++) + "", result.BSSID
+						+ "&" + result.level));
 			}
-			String str=null;
+			String str = null;
 			try {
-				hp.setEntity(new UrlEncodedFormEntity(param, "utf-8")); //jsonObj.toString()));
+				hp.setEntity(new UrlEncodedFormEntity(param, "utf-8")); // jsonObj.toString()));
 				// 发送请求
 				HttpResponse response = hc.execute(hp);
 				// 返回200即请求成功
@@ -215,7 +225,11 @@ public class QueryActivity extends Activity implements SensorEventListener{
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			//返回包好user信息的str
+			bar1.setProgress(100);
+			duration = System.currentTimeMillis() - startTime;
+			if (duration > 3000)
+				expectedTime = duration;
+			// 返回包好user信息的str
 			return str;
 		}
 
@@ -223,96 +237,103 @@ public class QueryActivity extends Activity implements SensorEventListener{
 		protected void onPostExecute(String result) {
 			// TODO Auto-generated method stub
 			super.onPostExecute(result);
-			//在文本框中显示user信息
+			// 在文本框中显示user信息
 			mTvResult.setText(result);
+			bar1.setVisibility(View.GONE);
 		}
 
 		@Override
-		protected void onProgressUpdate(Integer ... values) {
+		protected void onProgressUpdate(Integer... values) {
 			super.onProgressUpdate(values);
-			switch (values[0]) {
-				case 0: 
-					mTvResult.setText("正在扫描Wifi");
-					break;
-				case 1:
-					mTvResult.setText("扫描成功，正在查询");
-					break;
+			switch (values[1]) {
+			case 0:
+				mTvResult.setText("正在扫描Wifi");
+				break;
+			case 1:
+				mTvResult.setText("扫描成功，正在查询");
+				break;
 			}
+			bar1.setProgress((int) (values[0] / expectedTime * 100));
 		}
 
 		/**
 		 * 处理HttpResponse返回的response信息，从体重提取name, sex, age等信息
+		 * 
 		 * @param response
 		 * @return
 		 * @throws ParseException
 		 * @throws IOException
 		 * @throws JSONException
 		 */
-		String PhaseJson(HttpResponse response) throws ParseException, IOException, JSONException
-		{
+		String PhaseJson(HttpResponse response) throws ParseException,
+				IOException, JSONException {
 			String pos = null, x = null, y = null, str = null;
 			mStrResult = EntityUtils.toString(response.getEntity());
 			// 将返回结果生成JSON对象，返回的格式首先是user数组
 			JSONObject result = new JSONObject(mStrResult);
-			//从user数组中提取出user的信息
-			//对于同名用户，只获取最后一条信息 //改成了只获取第一条
-            pos = result.getString("pos" );
-            x = result.getString("x");
-            y = result.getString("y" );
-            str = "房间: " + pos + " X: " + x + " Y: " + y;
-            System.out.println(str);
-            
-            return str;
+			// 从user数组中提取出user的信息
+			// 对于同名用户，只获取最后一条信息 //改成了只获取第一条
+			pos = result.getString("pos");
+			x = result.getString("x");
+			y = result.getString("y");
+			str = "房间: " + pos + " X: " + x + " Y: " + y;
+			System.out.println(str);
+
+			return str;
 		}
+
 		class WifiReceiver extends BroadcastReceiver {
-	        @Override
-	        public void onReceive(Context context, Intent intent) {             
-	            finished = true;
-	        }
-	    }
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				finished = true;
+			}
+		}
 	}
-	
+
 	public void displayMap(double posX, double posY) {
 		BitmapFactory.Options myOptions = new BitmapFactory.Options();
-	    myOptions.inDither = true;
-	    myOptions.inScaled = false;
-	    myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
-	    myOptions.inPurgeable = true;
-	    
-	    Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.map,myOptions);
-	    Paint paint = new Paint();
-	    paint.setAntiAlias(true);
-	    paint.setColor(Color.RED);
-	    //paint.setStyle(Paint.Style.STROKE);
-	    //paint.setStrokeWidth(2);
-	    paint.setStyle(Paint.Style.FILL);
+		myOptions.inDither = true;
+		myOptions.inScaled = false;
+		myOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;// important
+		myOptions.inPurgeable = true;
 
-	    Bitmap workingBitmap = Bitmap.createBitmap(bitmap);
-	    Bitmap mutableBitmap = workingBitmap.copy(Bitmap.Config.ARGB_8888, true);
+		Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+				R.drawable.map, myOptions);
+		Paint paint = new Paint();
+		paint.setAntiAlias(true);
+		paint.setColor(Color.RED);
+		// paint.setStyle(Paint.Style.STROKE);
+		// paint.setStrokeWidth(2);
+		paint.setStyle(Paint.Style.FILL);
 
-	    Canvas canvas = new Canvas(mutableBitmap);
+		Bitmap workingBitmap = Bitmap.createBitmap(bitmap);
+		Bitmap mutableBitmap = workingBitmap
+				.copy(Bitmap.Config.ARGB_8888, true);
 
-	    orientOfMap = Float.parseFloat((inOrient).getText().toString());
-	    
-	    Matrix matrix = new Matrix();
-	    matrix.setRotate(orientOfDevice - orientOfMap + 180);
+		Canvas canvas = new Canvas(mutableBitmap);
 
-	    Path path = new Path();
-	    path.setFillType(Path.FillType.EVEN_ODD);
-	    path.moveTo(0, -20);
-	    path.lineTo(7, 0);
-	    path.lineTo(-7, 0);
-	    path.close();
-	    path.transform(matrix);
-	    path.offset(canvas.getWidth() * (float)posX, canvas.getHeight() * (float)posY);
-			    
-	    canvas.drawPath(path, paint);
-			    
-	    //offset is cumulative
-	    //next draw displaces 50,100 from previous
-	    ImageView imageView = (ImageView) findViewById(R.id.img_map);
-	    imageView.setAdjustViewBounds(true);
-	    imageView.setImageBitmap(mutableBitmap);
+		orientOfMap = Float.parseFloat((inOrient).getText().toString());
+
+		Matrix matrix = new Matrix();
+		matrix.setRotate(orientOfDevice - orientOfMap + 180);
+
+		Path path = new Path();
+		path.setFillType(Path.FillType.EVEN_ODD);
+		path.moveTo(0, -20);
+		path.lineTo(7, 0);
+		path.lineTo(-7, 0);
+		path.close();
+		path.transform(matrix);
+		path.offset(canvas.getWidth() * (float) posX, canvas.getHeight()
+				* (float) posY);
+
+		canvas.drawPath(path, paint);
+
+		// offset is cumulative
+		// next draw displaces 50,100 from previous
+		ImageView imageView = (ImageView) findViewById(R.id.img_map);
+		imageView.setAdjustViewBounds(true);
+		imageView.setImageBitmap(mutableBitmap);
 	}
 
 	@Override
@@ -321,10 +342,11 @@ public class QueryActivity extends Activity implements SensorEventListener{
 		float azimuth_angle = event.values[0];
 		orientOfDevice = azimuth_angle;
 		displayMap(0.5, 0.5);
-		//azimuth_angle is the angle between magnetic north and the device's y axis
-	    //float pitch_angle = event.values[1];
-	    //float roll_angle = event.values[2];
-	    // Do something with these orientation angles.
+		// azimuth_angle is the angle between magnetic north and the device's y
+		// axis
+		// float pitch_angle = event.values[1];
+		// float roll_angle = event.values[2];
+		// Do something with these orientation angles.
 	}
 
 	@Override
@@ -335,12 +357,13 @@ public class QueryActivity extends Activity implements SensorEventListener{
 	@Override
 	protected void onResume() {
 		super.onResume();
-	    mSensorManager.registerListener(this, mOrientation, SensorManager.SENSOR_DELAY_NORMAL);
+		mSensorManager.registerListener(this, mOrientation,
+				SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
 	@Override
 	protected void onPause() {
-	    super.onPause();
-	    mSensorManager.unregisterListener(this);
+		super.onPause();
+		mSensorManager.unregisterListener(this);
 	}
 }
